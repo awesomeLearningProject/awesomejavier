6a18ba702963b8f41547c5b1f5ec82b48de33c80
Payne <1289690157@qq.com>
2021-04-08 01:53:02 +0800

    init

--- src/com/payne/awesomejavier/stage1/Main.java
+++ src/com/payne/awesomejavier/stage1/Main.java
@@ -6,5 +6,6 @@ package com.payne.awesomejavier.stage1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello awesome javier");
+        System.out.println("Hello awesome javier");
     }
 }
